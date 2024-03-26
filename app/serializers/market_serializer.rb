0506b92fdd979807ea3ddf7a3c5000d923b4839c
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-26 14:27:33 -0600

    Remove call to #vendor_count

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,10 +1,5 @@
 class MarketSerializer 
   include JSONAPI::Serializer
 
-  attributes :name, :street, :city, :county, :state, :zip, :lat, :lon
-  attribute :vendor_count
-
-  def vendor_count
-    @market.get_vendor_count
-  end
+  attributes :vendor_count, :name, :street, :city, :county, :state, :zip, :lat, :lon
 end
