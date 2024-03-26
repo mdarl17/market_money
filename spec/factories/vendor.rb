55fd8c84576755fcc686a9fa432d4fa53e870f09
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-26 17:52:48 -0600

    Changed Vendor name so Amazon and Google were no longer running farmer's markets thoughout the country.  Phony data, but figured it was easy enough to give them the mom and pop names that are a touch more fitting.

--- spec/factories/vendor.rb
+++ spec/factories/vendor.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :vendor do
-    name { Faker::Commerce.vendor }
-    description { Faker::Commerce.department }
+    name { Faker::Company.name.concat(" Market") }
+    description { Faker::Company.industry }
     contact_name { Faker::Name.name }
     contact_phone { Faker::PhoneNumber.phone_number }
     credit_accepted { Faker::Boolean.boolean(true_ratio: 0.9) }
