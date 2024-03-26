dc436abb2aecc1008ab7fa16e0b382dcaf90f949
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-26 16:36:57 -0600

    Add attribute presence validation tests for all

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,7 @@
 class Vendor < ApplicationRecord 
+	validates :name, :description, :contact_name, :contact_phone
+	validates :credit_accepted, inclusion: { in: [true, false] }
+
 	has_many :market_vendors 
 	has_many :markets, through: :market_vendors
-	
 end
