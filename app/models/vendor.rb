92f3937c7eadea79450da86f0479bb8cb46ee86b
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-25 18:51:09 -0700

    feat: Add validations for all attributes for Vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,8 @@
 class Vendor < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :contact_name, presence: true
+  validates :contact_phone, presence: true
+  validates :credit_accepted, presence: true, inclusion: [true, false]
 
 end
---
+++ spec/models/vendor_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Vendor, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:contact_name) }
+    it { should validate_presence_of(:contact_phone) }
+    it { should validate_presence_of(:credit_accepted) }
+  end
+end
