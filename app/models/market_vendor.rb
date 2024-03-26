993e5826beee939caee2e332ac2f958120bc597d
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-26 17:44:51 -0600

    Add presence validations for market_it and vendor_id

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -2,5 +2,8 @@ class MarketVendor < ApplicationRecord
 	belongs_to :market
 	belongs_to :vendor
 
+	validates :market_id, presence: true
+	validates :market_id, numericality: { only_integer: true }
+	validates :vendor_id, presence: true
+	validates :vendor_id, numericality: { only_integer: true }
 end
-
