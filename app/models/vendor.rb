8e6c6fd39f429a9a88b67a3215a40c4314fdd185
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-26 17:49:24 -0600

    Remove presence_of validation for :credit_accepted.  Very strange behavior with shoulda_matchers. inclusion_of test gives a warning that false positives could potentially happen with 1s and 0s and possibly other values, but the tests all pass.
    After the warning I changed the test to the more simple presence_of and spent a LOT of time (unproductive struggle) trying to troubleshoot to see if I had an accidental keystroke or syntax is just off, not sure. I have a feeling it will take very little to make the 'presence' test pass, but I don't know what it is, and it's apparently vital enough to throw an error and stop execution.

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,10 @@
 class Vendor < ApplicationRecord 
-	validates :name, :description, :contact_name, :contact_phone
+	validates :name, :description, :contact_name, :contact_phone, presence: true
 	validates :credit_accepted, inclusion: { in: [true, false] }
-
+	
+	# See warning in respective RSpec test re: using `inclusion` matcher
+	# validates :credit_accepted, presence: true
+	
 	has_many :market_vendors 
 	has_many :markets, through: :market_vendors
 end
