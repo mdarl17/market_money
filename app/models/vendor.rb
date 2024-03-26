d6fb72cbfc5407e6d7b490f58fa030b10068ae57
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-25 19:44:45 -0700

    feat: Update validations to correctly validate boolean value of credit_accepted column

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,6 +3,13 @@ class Vendor < ApplicationRecord
   validates :description, presence: true
   validates :contact_name, presence: true
   validates :contact_phone, presence: true
-  validates :credit_accepted, presence: true, inclusion: [true, false]
+
+  validate :credit_accepted_is_boolean, on: :create
+  
+  def credit_accepted_is_boolean
+    unless credit_accepted == true || credit_accepted == false
+      errors.add(:credit_accepted, "must be true or false")
+    end
+  end
 
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -6,6 +6,40 @@ RSpec.describe Vendor, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:contact_name) }
     it { should validate_presence_of(:contact_phone) }
-    it { should validate_presence_of(:credit_accepted) }
+  end
+
+  it 'is valid only when true or false' do
+    vendor_params_1 = {
+    name: "Buzzy Bees",
+    description: "local honey and wax products",
+    contact_name: "Berly Couwer",
+    contact_phone: "8389928383",
+    credit_accepted: false
+    }
+
+    vendor_params_2 = {
+    name: "Buzzy Bees",
+    description: "local honey and wax products",
+    contact_name: "Berly Couwer",
+    contact_phone: "8389928383",
+    credit_accepted: true
+    }
+
+    vendor_params_3 = {
+    name: "Buzzy Bees",
+    description: "local honey and wax products",
+    contact_name: "Berly Couwer",
+    contact_phone: "8389928383",
+    credit_accepted: nil
+    }
+
+    vendor_1 = Vendor.new(vendor_params_1)
+    vendor_2 = Vendor.new(vendor_params_2)
+    vendor_3 = Vendor.new(vendor_params_3)
+
+
+    expect(vendor_1.save).to eq(true)
+    expect(vendor_2.save).to eq(true)
+    expect(vendor_3.save).to eq(false)
   end
 end
