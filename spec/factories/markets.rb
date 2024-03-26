2b455dfdb4cf0cef21e1b668d8439bb6c72af65e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-25 22:16:49 -0600

    New market factory for test data creation

---
+++ spec/factories/markets.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :market do
+    name { Faker::Company.name + " Market" }
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    county { Faker::Address.community }
+    zip { Faker::Address.zip_code }
+    lat { Faker::Address.latitude }
+    lon { Faker::Address.longitude }
+  end
+end
