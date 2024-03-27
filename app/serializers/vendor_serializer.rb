5cd125c637a91e95e25b2bc6ea798a3501715a61
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-27 16:24:40 -0700

    feat: Add new formating methods to VendorSerializer to serialize data for market vendors get request

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -15,4 +15,24 @@ class VendorSerializer
       }
     }
   end
+
+  def self.serialize_market_vendors(vendors)
+    {data: format_multiple_vendors(vendors)}
+  end
+
+  def self.format_multiple_vendors(vendors)
+    vendors.map do |vendor|
+      {
+        id: vendor.id.to_s,
+        type: "vendor",
+        attributes: {
+          name: vendor.name,
+          description: vendor.description,
+          contact_name: vendor.contact_name,
+          contact_phone: vendor.contact_phone,
+          credit_accepted: vendor.credit_accepted
+        }
+      }
+    end
+  end
 end
--- spec/serializers/vendor_serializer_spec.rb
+++ spec/serializers/vendor_serializer_spec.rb
@@ -7,40 +7,75 @@ RSpec.describe VendorSerializer do
     expect(serializer).to be_a(VendorSerializer)
   end
 
-  it 'can return data with the correct formatting' do
-    vendor = create(:vendor, credit_accepted: false)
+  describe '#self.format_vendor' do
+    it 'can return data with the correct formatting' do
+      vendor = create(:vendor, credit_accepted: false)
 
-    formatted_response = VendorSerializer.format_vendor(vendor)
+      formatted_response = VendorSerializer.format_vendor(vendor)
 
-    expect(formatted_response).to have_key(:data)
-    expect(formatted_response[:data]).to be_a(Hash)
+      expect(formatted_response).to have_key(:data)
+      expect(formatted_response[:data]).to be_a(Hash)
 
-    data = formatted_response[:data]
+      data = formatted_response[:data]
 
-    expect(data).to have_key(:id)
-    expect(data[:id]).to be_a(String)
+      expect(data).to have_key(:id)
+      expect(data[:id]).to be_a(String)
 
-    expect(data).to have_key(:type)
-    expect(data[:type]).to eq("vendor")
+      expect(data).to have_key(:type)
+      expect(data[:type]).to eq("vendor")
 
-    expect(data).to have_key(:attributes)
-    expect(data[:attributes]).to be_a(Hash)
+      expect(data).to have_key(:attributes)
+      expect(data[:attributes]).to be_a(Hash)
 
-    attributes = data[:attributes]
+      attributes = data[:attributes]
 
-    expect(attributes).to have_key(:name)
-    expect(attributes[:name]).to be_a(String)
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a(String)
 
-    expect(attributes).to have_key(:description)
-    expect(attributes[:description]).to be_a(String)
+      expect(attributes).to have_key(:description)
+      expect(attributes[:description]).to be_a(String)
 
-    expect(attributes).to have_key(:contact_name)
-    expect(attributes[:contact_name]).to be_a(String)
+      expect(attributes).to have_key(:contact_name)
+      expect(attributes[:contact_name]).to be_a(String)
 
-    expect(attributes).to have_key(:contact_phone)
-    expect(attributes[:contact_phone]).to be_a(String)
+      expect(attributes).to have_key(:contact_phone)
+      expect(attributes[:contact_phone]).to be_a(String)
 
-    expect(attributes).to have_key(:credit_accepted)
-    expect(attributes[:credit_accepted]).to eq(false)
+      expect(attributes).to have_key(:credit_accepted)
+      expect(attributes[:credit_accepted]).to eq(false)
+    end
+  end
+
+  describe '#self.serialize_market_vendors(market_vendors)' do
+    it 'will format the data correctly for market_vendors' do
+      vendors = create_list(:vendor, 3, credit_accepted: false)
+
+      formatted_vendors = VendorSerializer.serialize_market_vendors(vendors)
+
+      expect(formatted_vendors).to have_key(:data)
+      expect(formatted_vendors[:data]).to be_a(Array)
+
+      vendor_data = formatted_vendors[:data].first
+
+      expect(vendor_data).to have_key(:attributes)
+      expect(vendor_data[:attributes]).to be_a(Hash)
+
+      attributes = vendor_data[:attributes]
+
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a(String)
+
+      expect(attributes).to have_key(:description)
+      expect(attributes[:description]).to be_a(String)
+
+      expect(attributes).to have_key(:contact_name)
+      expect(attributes[:contact_name]).to be_a(String)
+
+      expect(attributes).to have_key(:contact_phone)
+      expect(attributes[:contact_phone]).to be_a(String)
+
+      expect(attributes).to have_key(:credit_accepted)
+      expect(attributes[:credit_accepted]).to eq(false)
+    end
   end
 end
