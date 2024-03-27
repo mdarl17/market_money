45a252cc2b69ef0f3b9755d29d79a7f5e16af776
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-27 01:13:23 -0600

    Add relationship tests with :markets and :market_vendors

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,7 @@
 class Vendor < ApplicationRecord
+  has_many :market_vendors
+  has_many :markets, through: :market_vendors
+
   validates :name, presence: true
   validates :description, presence: true
   validates :contact_name, presence: true
