4c009694b963e49c916e19317189a735c86dc231
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-27 18:28:20 -0700

    feat: Add condition to invalid record response to customize status code being returned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-	rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+	rescue_from with: :not_found_response
 	rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
 
   def not_found_response(exception)
@@ -7,6 +7,18 @@ class ApplicationController < ActionController::API
   end
 
   def invalid_record_response(exception)
-    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400)).serialize_json, status: 400
+    message = exception.message
+
+    if message.include?("association")
+      status_code = 422
+    elsif message.include?("must exist")
+      status_code = 404
+    else
+      status_code = 400
+    end
+    
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, status_code)).serialize_json, status: status_code
   end
+
+
 end
