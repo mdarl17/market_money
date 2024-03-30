ad8fff87e84d81248e21a9c9e441a812ab422444
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-30 16:12:01 -0600

    Non-restful controller action - #nearest_atms - per US-11; returns nearest ATM cash dispensers to a Market instance; ordered by distance ascending

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -17,12 +17,23 @@ class Api::V0::MarketsController < ApplicationController
 			if !params[:state] && params[:city]
 				raise StandardError, "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
 			end
-				results = Market.search_db(city: params[:city], state: params[:state], name: params[:name])
-				render json: MarketSerializer.new(results)
+
+			results = Market.search_db(city: params[:city], state: params[:state], name: params[:name])
+			render json: MarketSerializer.new(results)
+		
 		rescue StandardError => e
 			render json: ErrorSerializer.new(ErrorMessage.new(e.message, 422)), status: :unprocessable_entity
 		end
 	end	
+
+	def nearest_atms 
+		market = Market.find(params[:id])
+		lat = market.lat
+		lon = market.lon
+		facade = AtmFacade.new(lat: lat, lon: lon)
+		results = facade.nearby_atms
+		render json: AtmSerializer.new(results)
+	end 
 
 	private 
 	
