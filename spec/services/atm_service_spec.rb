4b8e2a37a11b79c674882da0c274b7c83224517a
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-30 18:53:48 -0600

    New service tests for nearby_atms endpoint requiring an external API call

--- spec/services/atm_service_spec.rb
+++ spec/services/atm_service_spec.rb
@@ -1,20 +1,34 @@
 require "rails_helper"
 
-RSpec.describe "ATM Service", type: :service do 
+RSpec.describe "ATM Service", :vcr, type: :service do 
 	before(:each) do 
 		load_test_data 
 		@service = AtmService.new
 	end
+
 	describe "#find_nearby_atms(lat, lon)" do 
 		it "given latitude and longitude coordinates of a Market, it returns a list of nearby ATM's" do
 			market = Market.all.sample
-			lat = market.lat
-			lon = market.lon
-
-			atm_list_hash = @service.find_nearby_atms(lat, lon)
+			atm_list_hash = @service.find_nearby_atms(market.lat, market.lon)
 			results_array = atm_list_hash[:results]
-			Atm
-			require 'pry'; binding.pry
+			
+			expect(results_array.first.keys).to include(:type, :position, :dist, :poi, :address)
+
+			expect(results_array.first[:type]).to be_a(String)
+
+			expect(results_array.first[:position]).to be_a(Hash)
+			expect(results_array.first[:position].keys).to match_array([:lat, :lon])
+			expect(results_array.first[:position][:lat]).to be_a(Float)
+			expect(results_array.first[:position][:lon]).to be_a(Float)
+			
+			expect(results_array.first[:dist]).to be_a(Float)
+			
+			expect(results_array.first[:poi]).to be_a(Hash)
+			expect(results_array.first[:poi].keys).to be_include(:name)
+
+			expect(results_array.first[:address]).to be_a(Hash)
+			expect(results_array.first[:address].keys).to include(:freeformAddress)
+			expect(results_array.first[:address][:freeformAddress]).to be_a(String)
 		end
 	end
 end 
