02d43f493aa92ab8e283e767668faf9be24c1f4f
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-30 23:37:32 -0600

    Add filter_sensitive_data config to VCR configuration to hide API key from being able to be seen in cassette recording metadata

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,6 +84,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data('<API_Key>') { Rails.application.credentials.tomtom[:key] }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
   config.default_cassette_options = { record: :new_episodes }
